992c59c2ed549d4158dda600b8fb6b91ab151b49
Youssef Saber <ysaber@e1r2p3.1337.ma>
2023-11-24 17:16:46 +0100

    get_next_line

---
+++ get_next_line.c
@@ -0,0 +1,129 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysaber <ysaber@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 16:28:43 by ysaber            #+#    #+#             */
+/*   Updated: 2023/11/24 16:28:43 by ysaber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*next_line(int fd, char *str)
+{
+	char	*buff;
+	int		i;
+
+	i = 1;
+	buff = malloc(BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	while (!ft_strchr(str, '\n') && i != 0)
+	{
+		i = read(fd, buff, BUFFER_SIZE);
+		if (i == -1)
+		{
+			free(buff);
+			free(str);
+			return (NULL);
+		}
+		buff[i] = '\0';
+		str = ft_strjoin(str, buff);
+		if (!str)
+			return (NULL);
+	}
+	free(buff);
+	return (str);
+}
+
+char	*jib_star(char *str)
+{
+	char	*s;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!str[0])
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i])
+		i++;
+	s = malloc(i + 1);
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		s[j++] = str[i++];
+	if (str[i] == '\n')
+	{
+		s[j] = '\n';
+		j++;
+	}
+	s[j] = '\0';
+	return (s);
+}
+
+char	*haha(char *str)
+{
+	char	*s;
+	int		j;
+	size_t	i;
+
+	i = 0;
+	j = 0;
+	if (!str)
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\0')
+	{
+		free(str);
+		return (NULL);
+	}
+	s = malloc(ft_strlen(str) - i + 1);
+	if (!s)
+		return (NULL);
+	if (str[i] == '\n')
+		i++;
+	while (str[i] != '\0')
+		s[j++] = str[i++];
+	s[j] = '\0';
+	free(str);
+	return (s);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str;
+	char		*s;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	str = next_line(fd, str);
+	if (!str)
+		return (NULL);
+	s = jib_star(str);
+	str = haha(str);
+	return (s);
+}
+
+// int main(void) {
+//     int fd;
+//     char *line;
+
+//     fd = open("haha.txt", O_RDONLY);
+
+//     while ((line = get_next_line(fd)) != NULL) {
+//         printf("%s\n", line);
+//         free(line);
+//     }
+
+//     close(fd);
+
+//     return (0);
+// }
---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysaber <ysaber@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 16:28:45 by ysaber            #+#    #+#             */
+/*   Updated: 2023/11/24 16:28:45 by ysaber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+size_t	ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*get_next_line(int fd);
+
+#endif
